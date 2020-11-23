1804993f8c264acfb719095e201d3f7ff28f524c
nomurad_home_wsl_Ubuntu18 <nomurad@outlook.jp>
2020-11-23 22:09:40 +0900

    for module debuging

---
+++ src/debug.f90
@@ -0,0 +1,10 @@
+program debug 
+    use mod_vector 
+    use mod_calcutil 
+    use mod_reentry_calc 
+    implicit none 
+
+    print *, "reentry parameters:"
+    print *, reentry_params
+
+end program
