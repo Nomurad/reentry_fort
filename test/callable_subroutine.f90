476674771797edd84891389a9aa9ee67e4772a61
Seiya Nomura <nomurad@outlook.jp>
2020-12-01 12:05:27 +0900

    modulize

--- test/callable_subroutine.f90
+++ test/callable_subroutine.f90
@@ -1,35 +1,43 @@
-subroutine test(n,A) bind(c)
-    use iso_c_binding
-    implicit none
-    integer(c_int), intent(in) :: n 
-    real(8), intent(inout) :: A(n,n)
-    real(8), allocatable :: AT(:,:)
-    real(8), allocatable :: B(:)
-    real(8) :: dat = 100d0
-    integer(c_int) i, j
+module testing
+    implicit none 
+    type mytype
+        integer :: i = 100
+    end type
 
-    AT = (A)
+    contains
+    subroutine test(n,A) bind(c)
+        use iso_c_binding
+        implicit none
+        integer(c_int), intent(in) :: n 
+        real(c_double), intent(inout) :: A(n,n)
+        real(c_double), allocatable :: AT(:,:)
+        real(c_double), allocatable :: B(:)
+        real(c_double) :: dat = 100d0
+        integer(c_int) i, j
 
-    write(*, *) "** write from fortran"
-    write(*, *) n 
-    do i = 1, n
-        write(*, *) (AT(i, j), j=1,n)
-    end do
+        AT = (A)
 
-    AT = AT * 10
-    A = AT
+        write(*, *) "** write from fortran"
+        write(*, *) n 
+        do i = 1, n
+            write(*, "(3f10.5)") (AT(i, j), j=1,n)
+        end do
 
-    allocate(B(n*n))
-    open(20, file="input.dat", form="unformatted")
-    read(20) B
-    close(20)
-    print *, "read dat"
-    write(*, "(3f10.5)") reshape(B, [3,3])
-    print *, "** end fortran"
+        AT = AT * 10
+        A = AT
 
-    open(20, file="output.dat", form="unformatted")
-    write(20) AT
-    close(20)
+        allocate(B(n*n))
+        open(20, file="input.dat", form="unformatted")
+        read(20) B
+        close(20)
+        print *, "read dat"
+        write(*, "(3f10.5)") reshape(B, [3,3])
+        print *, "** end fortran"
 
-end subroutine
+        open(20, file="output.dat", form="unformatted")
+        write(20) AT
+        close(20)
 
+    end subroutine
+
+end module
